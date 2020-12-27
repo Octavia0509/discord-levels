8b73289a4b41c25dad2b5120f651568ffbd3502e
Oϲτανια <lcsdesprez@gmail.com>
2020-12-27 15:42:33 +0100

    Ajout du fichier index.d.ts compilé

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Ajoute de l'expérience à l'utilisateur
+ * @param {any} message Paramètre de votre événnement message
+ * @param {string} userID L'identifiant de l'utilisateur
+ * @param {number} XP L'expérience qui sera ajoutée
+ * @returns {any}
+ */
+export declare function addXP(message: any, userID: string, XP: number): any;
