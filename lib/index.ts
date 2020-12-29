2b5063431d16f01f982591ebb42b394ecbab1b99
Oϲτανια <lcsdesprez@gmail.com>
2020-12-29 21:36:17 +0100

    Nouvelle version: RANKCARD

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import * as db from 'quick.db'
-import { MessageEmbed } from 'discord.js'
+import { MessageEmbed, MessageAttachment } from 'discord.js'
+import * as canvas from 'canvas-constructor'
 
 /**
  * Adds experience to the user
@@ -82,9 +83,9 @@ export function leaderboard (client: any, message: any) : any {
 
     if(!message) throw new Error("You must enter a message parameter, referring to your 'message' event");
 
-    let data = db.all().filter(i => i.ID.startsWith("xp_")).sort((a, b) => b.data - a.data);
+    let data = db.all().filter(i => i.ID.startsWith("level_")).sort((a, b) => b.data - a.data);
     if(data.length < 1) return message.channel.send("It seems that there is no data");
-    let authorRank = data.map(i => i.ID).indexOf(`xp_${message.guild.id}_${message.author.id}`) + 1 || "N/A";
+    let authorRank = data.map(i => i.ID).indexOf(`level_${message.guild.id}_${message.author.id}`) + 1 || "N/A";
 
     let leaderboard = [];
     for (let i in data) {
@@ -116,3 +117,70 @@ export function leaderboard (client: any, message: any) : any {
     message.channel.send(embed);
 
 };
+
+/**
+ * Returns a rankcard
+ * @param {any} message Parameter of your event message
+ * @param {string} userID The ID of the user
+ * @returns {any}
+ */
+export function Rankcard (message: any, userID: string) : any {
+    if(!message) throw new Error("You must enter a message parameter, referring to your 'message' event");
+    
+    if(!userID) throw new Error("You must enter a userID parameter, corresponds to the ID of the user");
+    if(typeof userID !== "string") throw new Error("The userID parameter must be a String");
+
+    message.guild.members.fetch(userID).then(async (m: any) => {
+        let avatar = await canvas.resolveImage(m.user.displayAvatarURL({ dynamic: false, format: "png" }));
+
+        const data = {
+            level: db.get(`level_${message.guild.id}_${userID}`) || 1,
+            xp: db.get(`xp_${message.guild.id}_${userID}`) || 1
+        };
+
+        let status = await canvas.resolveImage(`assets/status_${m.user.presence.status}.png`);
+        let black = await canvas.resolveImage('assets/black.png');
+
+        let userRank = db.all().filter(i => i.ID.startsWith("level_")).sort((a, b) => b.data - a.data).map(i => i.ID).indexOf(`level_${message.guild.id}_${userID}`) + 1 || "N/A";
+
+        let image = new canvas.Canvas(500, 200)
+            .setColor("#303133")
+            .printRectangle(0, 0, 500, 200)
+            .setColor("#fcfdff")
+            .setTextAlign("center")
+            .setTextSize(35)
+            .printText((m.displayName.length > 15 ? m.displayName.substring(0, 12) + "..." : m.displayName) + "#" + m.user.discriminator, 250, 45)
+            .printCircularImage(avatar, 70, 110, 50)
+            .printCircularImage(black, 100, 140, 15)
+            .printCircularImage(status, 100, 140, 20)
+            .setColor(String(m.user.presence.status).replace("online", "#43b581").replace("idle", "#faa61a").replace("dnd", "#f04747").replace("offline", "#747f8d"))
+            .printRoundedRectangle(130, 95, 350, 45, 15)
+            .setColor("#303133")
+            .printRoundedRectangle(132, 97, 346, 42, 15)
+            .setColor("#fcfdff")
+            .printRoundedRectangle(134, 99, ((data.xp/100)*340), 38, 15)
+            .setColor("#fcfdff")
+            .setTextAlign("left")
+            .setTextSize(19)
+            .printText(`Level : ${data.level} | XP : ${data.xp} / 100`, 10, 190);
+
+        let colorRanks = {
+            1: '#d1b94f',
+            2: '#adaca6',
+            3: '#96531d'
+        };
+
+        let color = (userRank === 1 || userRank === 2 || userRank === 3) ? colorRanks[userRank] : "#fcfdff"
+        let size = image.measureText(`Rank #${userRank}`).width;
+        await image.setColor(color).setTextAlign("left").setTextSize(21).printText(`Rank #${userRank}`, image.width-(size+20), 190)
+
+        if(userRank === 1 || userRank === 2 || userRank === 3) {
+            let userRank_img = await canvas.resolveImage(`assets/rank_${userRank}.png`);
+            await image.printImage(userRank_img, 435, 8);
+        };
+
+        let attachment = new MessageAttachment(image.toBuffer(), "rank.png");
+
+        message.channel.send(attachment)
+    }).catch(() => message.channel.send(":x: Unknown user ID."));
+}
